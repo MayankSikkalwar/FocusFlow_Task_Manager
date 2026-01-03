d5dfc82fe4ccc00189ee848ee26d8919902b0724
MayankSikkalwar <mayanksikkalwar576@gmail.com>
2026-01-03 22:08:25 +0530

    Phase 2: Backend core APIs with MongoDB and MVC

---
+++ backend/src/controllers/taskController.js
@@ -0,0 +1,21 @@
+import Task from "../models/Task.js";
+
+// CREATE TASK
+export const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// GET ALL TASKS
+export const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,31 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
-// Create express app
+
+
+dotenv.config();
+
 const app = express();
+
+// Connect database
+connectDB();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
 
 // Test route
 app.get("/", (req, res) => {
   res.send("FocusFlow Backend is running 🚀");
 });
 
-// Start server
-const PORT = 5000;
+app.use("/api/tasks", taskRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
